31e3edd4049e949d307453d2df51dc1d91d317f1
Pan Ziyue <random.rrr3@gmail.com>
2024-07-25 16:12:31 +0800

    updated PID code

--- temperature_with_pid/temperature_with_pid.ino
+++ temperature_with_pid/temperature_with_pid.ino
@@ -1,8 +1,3 @@
-/*
-   The value of Vout is obtained by averaging 1024 values reading  the voltage on pin A0. which is connected to the sensor output. 
-   The value of temeprature  ta, read by the sensor is shown on the serial monitor.
-*/
-
 #include "PID_v1.h"
 
 // Pin definition
@@ -13,8 +8,9 @@
 const double TARGET_TEMPERATURE = 13.5;
 const long CONTROL_LOOP_INTERVAL_MICROSEC = 100000; // 100ms or 10Hz control loop
 const long PRINT_INTERVAL = 500; // 0.5 second interval
-const double Kp=149.2088, Ki=1.8315, Kd=20.367; // PID version
+// const double Kp=149.2088, Ki=1.8315, Kd=20.367; // PID version
 // const double Kp=111.9065, Ki=1.0989, Kd=0;   // PI version
+const double Kp=8, Ki=0.1, Kd=0; // Hand tuned version
 
 // Calibration constants for temperature sensor, consult datasheet and adjust accordingly
 const double CALIB_VOUT0 = 0.64;   // Output voltage in V when sensor is at 0˚C
@@ -25,20 +21,21 @@ unsigned long previousMillis = 0UL;
 double temperature_err = -11.5;
 double temperature_last = 25;
 double voltage_output = 0.0;
+double start_time_seconds = 0.0;
 
 // input, output, setpoint, target
 PID myPID(&temperature_last, &voltage_output, &TARGET_TEMPERATURE, Kp, Ki, Kd, REVERSE);
 
 void senseTemperature() {
-  // For every run of the loop, compute a temperature average of 1023 values
+  // For every run of the loop, compute a temperature average of 511 values
   double voltage_average_sum = 0.0;
   double voltage = 0.0;
-  for (int i = 0; i < 1024; i++) {
+  for (int i = 0; i < 512; i++) {
     double sensor_raw_value = analogRead(TEMPERATURE_PIN);
-    voltage = (double) sensor_raw_value * (3.315 / 1023.0);
+    voltage = (double) sensor_raw_value * (1.05 / 1023.0);
     voltage_average_sum = voltage_average_sum + voltage;
   }
-  voltage = voltage_average_sum / 1023;
+  voltage = voltage_average_sum / 511;
   double temperature = (voltage - CALIB_VOUT0) / CALIB_TC;
 
   // Calculate temperature parameters that might be useful in doing PID control later
@@ -55,14 +52,17 @@ void setVoltage(double voltage) {
 
 // MAIN PROGRAM
 void setup() {
-  analogReference(EXTERNAL);
+  analogReference(INTERNAL);
   Serial.begin(9600);
   pinMode(MOSFET_PIN, OUTPUT);
   
+  delay(2000);
   myPID.SetOutputLimits(0.0,12.0);
   myPID.SetSampleTime(CONTROL_LOOP_INTERVAL_MICROSEC/1000); // in milliseconds
   myPID.SetTunings(Kp, Ki, Kd);
   myPID.SetMode(AUTOMATIC); // turn PID on
+  Serial.println("Started!");
+  start_time_seconds = millis()/1000.0;
 }
 
 // Task: find out how long each loop takes because PID is dependent on time taken per loop
@@ -77,14 +77,16 @@ void loop() {
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= PRINT_INTERVAL) {
     previousMillis = currentMillis;
-    // Serial.print("current temperature:");
-    Serial.print(temperature_last);
+    // char strbuffer[8];
+    // dtostrf(, 7, 1, strbuffer);
+    Serial.print(millis()/1000.0 - start_time_seconds); // Current time in seconds
     Serial.print("," );
-    // Serial.print("temperature error:");
-    Serial.print(temperature_err);
+    Serial.print(temperature_last); // Last temperature reading
+    Serial.print("," );
+    Serial.print(temperature_err); // Temperature error
     Serial.print("," );
     // Serial.print("voltage:");
-    Serial.print(voltage_output);
+    Serial.print(voltage_output); // Voltage of the peltier from 0 to 12V
     Serial.print("\n");
   }
 }
